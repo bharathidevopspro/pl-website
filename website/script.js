63e06303d5375528b4ca43eff779db9cb24565d3
bharathidevopspro <bharathi.devops.pro@gmail.com>
2026-04-05 18:56:54 +0530

    Update script.js

--- website/script.js
+++ website/script.js
@@ -3,16 +3,16 @@ function setMood(mood) {
 
   if (mood === "happy") {
     document.body.style.background = "#facc15";
-    message.innerText = "😊 Stay happy and keep smiling!";
+    message.innerText = " Stay happy and keep smiling!";
   }
 
   else if (mood === "sad") {
     document.body.style.background = "#1e3a8a";
-    message.innerText = "😔 It's okay, better days are coming!";
+    message.innerText = "It's okay, better days are coming!";
   }
 
   else if (mood === "chill") {
     document.body.style.background = "#0f766e";
-    message.innerText = "😎 Relax and enjoy your time!";
+    message.innerText = " Relax and enjoy your time!";
   }
 }
