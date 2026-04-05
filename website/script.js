4ea81c1689b2e30be5db1f96adbcb18c09e95067
unknown <bharathi@gmail.com>
2026-04-05 12:20:41 +0530

    web

---
+++ website/script.js
@@ -0,0 +1,18 @@
+function setMood(mood) {
+  let message = document.getElementById("message");
+
+  if (mood === "happy") {
+    document.body.style.background = "#facc15";
+    message.innerText = "😊 Stay happy and keep smiling!";
+  }
+
+  else if (mood === "sad") {
+    document.body.style.background = "#1e3a8a";
+    message.innerText = "😔 It's okay, better days are coming!";
+  }
+
+  else if (mood === "chill") {
+    document.body.style.background = "#0f766e";
+    message.innerText = "😎 Relax and enjoy your time!";
+  }
+}
